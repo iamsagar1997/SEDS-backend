f65d0d0a0d80d3464e5038e56987ac33d52159c6
Sagar Sapkota <you@example.com>
2022-06-16 18:28:21 +0545

    jwt

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls', namespace='post')),
     path('api/', include('api.urls', namespace='api')),
+    path('api/user', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
