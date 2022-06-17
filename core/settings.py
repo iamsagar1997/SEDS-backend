8f75285b964f57d69359c53582d548d7f3b39f13
Sagar Sapkota <you@example.com>
2022-06-17 08:17:58 +0545

    jwt completed

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'api',
     'corsheaders',
     'users',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -149,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = "users.NewUser" 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from users.models import NewUser
 
-
 class CustomUserSerializer(serializers.ModelSerializer):
     """
     Currently unused in preference of the below.
