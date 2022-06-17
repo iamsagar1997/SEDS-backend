b7534177fa673623176c4c1962b5cfc7f002f5b2
Sagar Sapkota <you@example.com>
2022-06-17 16:29:51 +0545

    filter and search

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from post.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticated, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
 from rest_framework import viewsets
 from rest_framework import filters
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework import filters
 
 class PostUserWritePermission(BasePermission):
     message = 'Editing post is restricted to author only'
@@ -17,43 +19,50 @@ class PostUserWritePermission(BasePermission):
         return obj.author == request.user
 
 
-class PostList(viewsets.ModelViewSet):
+class PostList(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
-    
-    def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get('pk')
-        return get_object_or_404(Post, slug=item)
-        
-    #Custom Query Set 
     def get_queryset(self):
-        return Post.objects.all()
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+    
+        
+class PostDetail(generics.RetrieveAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        slug = self.request.query_params.get('slug', None)
+        print(slug)
+        return Post.objects.filter(slug=slug)
+
+class PostListDetailfilter(generics.ListAPIView):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^slug']
             
 
+class PostSearch(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^slug']
 
-
-
-
-# """ class PostList(generics.ListCreateAPIView):
+    
+# class PostList(viewsets.ViewSet):
 #     permission_classes = [IsAuthenticated]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+#     queryset = Post.postobjects.all()
 
+#     def list(self, request):
+#         serializer_class = PostSerializer(self.queryset, many=True)
+#         return Response(serializer_class.data)
 
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-#     permission_classes = [PostUserWritePermission]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+#     def retrieve(self, request, pk=None):
+#         post = get_object_or_404(self.queryset, pk=pk)
+#         serializer_class = PostSerializer(post)
+#         return Response(serializer_class.data)
 
-
-# class EventList(generics.ListCreateAPIView):
-#     permission_classes = [IsAuthenticated]
-#     queryset = Post.objects.all()
-#     serializer_class = EventSerializer
-
-
-# class EventDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-#     permission_classes = [PostUserWritePermission]
-#     queryset = Post.objects.all()
-#     serializer_class = EventSerializer """
